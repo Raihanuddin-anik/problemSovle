0489782e04dccdc81953ef9b6180266ea0a95779
Raihanuddin-anik <67515498+Raihanuddin-anik@users.noreply.github.com>
2021-06-21 22:32:21 +0600

    submit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function rotLeft(arr, rotations) {
+function rotLeft(arr, rotate) {
     const rotatedArray = arr.concat();
-    for (let i = 0; i < rotations; i++) {
+    for (let i = 0; i < rotate; i++) {
       const frontItem = rotatedArray.shift();
       rotatedArray.push(frontItem);
     }
     return rotatedArray;
   }
   
-  const numRotation = 4;
-  const sampleArray = [1, 2, 3, 4, 5];
+  const num = 4;
+  const Arr = [1, 2, 3, 4, 5];
   
-console.log(rotLeft(sampleArray, numRotation));
+console.log(rotLeft(Arr, num));
