54c3f009ae7006d40a5094689e848183619f6f54
Raihanuddin-anik <67515498+Raihanuddin-anik@users.noreply.github.com>
2021-06-21 22:29:00 +0600

    problem

---
+++ index.js
@@ -0,0 +1,13 @@
+function rotLeft(arr, rotations) {
+    const rotatedArray = arr.concat();
+    for (let i = 0; i < rotations; i++) {
+      const frontItem = rotatedArray.shift();
+      rotatedArray.push(frontItem);
+    }
+    return rotatedArray;
+  }
+  
+  const numRotation = 4;
+  const sampleArray = [1, 2, 3, 4, 5];
+  
+console.log(rotLeft(sampleArray, numRotation));
